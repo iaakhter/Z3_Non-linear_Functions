Fix leakage and linear constraints bug for schmitt trigger

dRealRambus.py
@@ -50,7 +50,7 @@ def rambusOscillatorTanhDebug(a, numStages, hyper, g_cc = 0.5):
 		
 		#print ("f_sat")
 		#print (f_sat)
-		result = CheckSatisfiability(f_sat, 1e-16)
+		result = CheckSatisfiability(f_sat, 1e-12)
 		#print (result)
 		if result is None:
 			break
@@ -91,6 +91,7 @@ def rambusOscillatorTanhDebug(a, numStages, hyper, g_cc = 0.5):
 		print ("")
 
 def rambusOscillatorTanh(a, numStages, numSolutions = "all", g_cc = 0.5):
+	epsilon = 1e-12
 	model = TanhModel(modelParam = a, g_cc = g_cc, g_fwd = 1.0, numStages=numStages)
 	start = time.time()
 	g_fwd = 1.0
@@ -138,17 +139,18 @@ def rambusOscillatorTanh(a, numStages, numSolutions = "all", g_cc = 0.5):
 		
 		#print ("f_sat")
 		#print (f_sat)
-		result = CheckSatisfiability(f_sat, 1e-16)
-		#print (result)
+		result = CheckSatisfiability(f_sat, epsilon)
+		print (result)
 		if result is None:
 			break
 		hyper = np.zeros((lenV,2))
 		for i in range(lenV):
-			hyper[i,:] = [result[vs[i]].lb(), result[vs[i]].ub()]
+			hyper[i,:] = [result[vs[i]].lb() - 2*epsilon, result[vs[i]].ub() + 2*epsilon]
 
-		#allSolutions.append(hyper)
+		print ("hyper", hyper)
+		allSolutions.append(hyper)
 		# find the biggest hyperrectangle containing the unique solution
-		hyperWithUniqueSoln = np.zeros((lenV,2))
+		'''hyperWithUniqueSoln = np.zeros((lenV,2))
 		diff = np.ones((lenV))*0.4
 		startingIndex = 0
 		while True:
@@ -165,7 +167,7 @@ def rambusOscillatorTanh(a, numStages, numSolutions = "all", g_cc = 0.5):
 				allSolutions.append(hyperWithUniqueSoln)
 				break
 
-		print ("num solutions found", len(allSolutions))
+		print ("num solutions found", len(allSolutions))'''
 		'''if len(allSolutions) == 3:
 			break'''
 		
@@ -212,12 +214,11 @@ def mosfetCurrent(Vtp, Vtn, Vdd, Kn, Kp, Sn, Sp, IVarN, IVarP, VinVar, VoutVar):
 
 
 def rambusOscillatorMosfet(Vtp, Vtn, Vdd, Kn, Kp, Sn, numStages, numSolutions = "all", g_cc = 0.5):
+	model = MosfetModel(modelParam = [Vtp, Vtn, Vdd, Kn, Kp, Sn], g_cc = g_cc, g_fwd = g_fwd, numStages = numStages)
 	start = time.time()
 	print ("Vtp", Vtp, "Vtn", Vtn, "Vdd", Vdd, "Kn", Kn, "Kp", Kp, "Sn", Sn)
 	g_fwd = 1.0
 	Sp = Sn *2.0
-	model = MosfetModel(modelParam = [Vtp, Vtn, Vdd, Kn, Kp, Sn], g_cc = g_cc, g_fwd = g_fwd, numStages = numStages)
-	boundMap = model.boundMap
 	lenV = numStages*2
 
 	vs = []
@@ -238,8 +239,8 @@ def rambusOscillatorMosfet(Vtp, Vtn, Vdd, Kn, Kp, Sn, numStages, numSolutions = 
 			break
 		allConstraints = []	
 		for i in range(lenV):
-			allConstraints.append(vs[i] >= boundMap[i][0][0])
-			allConstraints.append(vs[i] <= boundMap[i][1][1])
+			allConstraints.append(vs[i] >= 0.0)
+			allConstraints.append(vs[i] <= Vdd)
 			allConstraints.append(g_fwd*(-ifwdNs[i]-ifwdPs[i]) + g_cc*(-iccNs[i]-iccPs[i]) == 0)
 			fwdInd = (i-1)%lenV
 			ccInd = (i+lenV//2)%lenV
@@ -265,7 +266,7 @@ def rambusOscillatorMosfet(Vtp, Vtn, Vdd, Kn, Kp, Sn, numStages, numSolutions = 
 		
 		#print ("f_sat")
 		#print (f_sat)
-		result = CheckSatisfiability(f_sat, 1e-16)
+		result = CheckSatisfiability(f_sat, 1e-12)
 		#print (result)
 		if result is None:
 			break
@@ -275,6 +276,7 @@ def rambusOscillatorMosfet(Vtp, Vtn, Vdd, Kn, Kp, Sn, numStages, numSolutions = 
 
 		#print ("hyper", hyper)
 		# find the biggest hyperrectangle containing the unique solution
+		boundMap = model.boundMap
 		hyperWithUniqueSoln = np.zeros((lenV,2))
 		diff = np.ones((lenV))*0.4
 		startingIndex = 0
@@ -316,7 +318,7 @@ def rambusOscillatorMosfet(Vtp, Vtn, Vdd, Kn, Kp, Sn, numStages, numSolutions = 
 	print ("time taken", end - start)
 
 
-rambusOscillatorTanh(a = -5.0, numStages = 2, numSolutions = 1, g_cc = 0.5)
+rambusOscillatorTanh(a = -5.0, numStages = 4, numSolutions = "all", g_cc = 4.0)
 
 #check if you can easily find the solution in the given hyper
 '''hyper = np.array([[-0.74, -0.72],

dRealSchmitt.py
@@ -11,52 +11,51 @@ import intervalUtils
 
 def nFet(Vtn, Vdd, Kn, Sn, gn, src, gate, drain, tI):
 	constraints = []
-	InLeak = (drain - src)*(1 + (gate - src - Vtn)/Vdd)*(gn/1e-4)
 	if type(src) == Variable or type(gate) == Variable:
-		constraints.append(logical_or(logical_and(src > drain, drain - src <= Vtn, tI == InLeak),
-										logical_and(src > drain, gate - src >= drain - src - Vtn, tI == -0.5*Sn*Kn*(drain - src - Vtn)*(drain - src - Vtn) + InLeak),
-										logical_and(src > drain, gate - src <= drain - src - Vtn, tI == -Sn*Kn*(drain - src - Vtn - (gate - src)/2.0)*(gate - src) + InLeak),
-										logical_and(src <= drain, gate - src <= Vtn, tI == InLeak),
-										logical_and(src <= drain, drain - src >= gate - src - Vtn, tI == 0.5*Sn*Kn*(gate - src - Vtn)*(gate - src - Vtn) + InLeak),
-										logical_and(src <= drain, drain - src <= gate - src - Vtn, tI == Sn*Kn*(gate - src - Vtn - (drain - src)/2.0)*(drain - src) + InLeak)))
+		constraints.append(logical_or(logical_and(src > drain, gate - drain <= Vtn, tI == -(src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+										logical_and(src > drain, gate - drain >= Vtn, src - drain >= gate - drain - Vtn, tI == -0.5*Sn*Kn*(gate - drain - Vtn)*(gate - drain - Vtn) - (src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+										logical_and(src > drain, gate - drain >= Vtn, src - drain <= gate - drain - Vtn, tI == -Sn*Kn*(gate - drain - Vtn - (src - drain)/2.0)*(src - drain) - (src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+										logical_and(src <= drain, gate - src <= Vtn, tI == (drain - src)*(2 + (gate - src - Vtn)/Vdd)*(gn*1e-4)),
+										logical_and(src <= drain, gate - src >= Vtn, drain - src >= gate - src - Vtn, tI == 0.5*Sn*Kn*(gate - src - Vtn)*(gate - src - Vtn) + (drain - src)*(2 + (gate - src - Vtn)/Vdd)*(gn*1e-4)),
+										logical_and(src <= drain, gate - src >= Vtn, drain - src <= gate - src - Vtn, tI == Sn*Kn*(gate - src - Vtn - (drain - src)/2.0)*(drain - src) + (drain - src)*(2 + (gate - src - Vtn)/Vdd)*(gn*1e-4))))
 	else:
 		if gate - src <= Vtn:
-			constraints.append(logical_or(logical_and(src > drain, drain - src <= Vtn, tI == InLeak),
-											logical_and(src > drain, gate - src >= drain - src - Vtn, tI == -0.5*Sn*Kn*(drain - src - Vtn)*(drain - src - Vtn) + InLeak),
-											logical_and(src > drain, gate - src <= drain - src - Vtn, tI == -Sn*Kn*(drain - src - Vtn - (gate - src)/2.0)*(gate - src) + InLeak),
-											logical_and(src <= drain, tI == InLeak)))
+			constraints.append(logical_or(logical_and(src > drain, gate - drain <= Vtn, tI == -(src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+											logical_and(src > drain, gate - drain >= Vtn, src - drain >= gate - drain - Vtn, tI == -0.5*Sn*Kn*(gate - drain - Vtn)*(gate - drain - Vtn) - (src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+											logical_and(src > drain, gate - drain >= Vtn, src - drain <= gate - drain - Vtn, tI == -Sn*Kn*(gate - drain - Vtn - (src - drain)/2.0)*(src - drain) - (src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+											logical_and(src <= drain, tI == (drain - src)*(2 + (gate - src - Vtn)/Vdd)*(gn*1e-4))))
 		else:
-			constraints.append(logical_or(logical_and(src > drain, drain - src <= Vtn, tI == InLeak),
-											logical_and(src > drain, gate - src >= drain - src - Vtn, tI == -0.5*Sn*Kn*(drain - src - Vtn)*(drain - src - Vtn) + InLeak),
-											logical_and(src > drain, gate - src <= drain - src - Vtn, tI == -Sn*Kn*(drain - src - Vtn - (gate - src)/2.0)*(gate - src) + InLeak),
-											logical_and(src <= drain, drain - src >= gate - src - Vtn, tI == 0.5*Sn*Kn*(gate - src - Vtn)*(gate - src - Vtn) + InLeak),
-											logical_and(src <= drain, drain - src <= gate - src - Vtn, tI == Sn*Kn*(gate - src - Vtn - (drain - src)/2.0)*(drain - src) + InLeak)))
+			constraints.append(logical_or(logical_and(src > drain, gate - drain <= Vtn, tI == -(src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+											logical_and(src > drain, gate - drain >= Vtn, src - drain >= gate - drain - Vtn, tI == -0.5*Sn*Kn*(gate - drain - Vtn)*(gate - drain - Vtn) - (src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+											logical_and(src > drain, gate - drain >= Vtn, src - drain <= gate - drain - Vtn, tI == -Sn*Kn*(gate - drain - Vtn - (src - drain)/2.0)*(src - drain) - (src - drain)*(2 + (gate - drain - Vtn)/Vdd)*(gn*1e-4)),
+											logical_and(src <= drain, drain - src >= gate - src - Vtn, tI == 0.5*Sn*Kn*(gate - src - Vtn)*(gate - src - Vtn) + (drain - src)*(2 + (gate - src - Vtn)/Vdd)*(gn*1e-4)),
+											logical_and(src <= drain, drain - src <= gate - src - Vtn, tI == Sn*Kn*(gate - src - Vtn - (drain - src)/2.0)*(drain - src) + (drain - src)*(2 + (gate - src - Vtn)/Vdd)*(gn*1e-4))))
+	
 	return constraints
 
 	return constraints
 def pFet(Vtp, Vdd, Kp, Sp, gp, src, gate, drain, tI):
 	constraints = []
-	IpLeak = (drain - src)*(1 + (gate - src - Vtp)/Vdd)*(gp/1e-4)
 	if type(src) == Variable or type(gate) == Variable:
-		constraints.append(logical_or(logical_and(src < drain, drain - src >= Vtp, tI == IpLeak),
-										logical_and(src < drain, gate - src <= drain - src - Vtp, tI == -0.5*Sp*Kp*(drain - src - Vtp)*(drain - src - Vtp) + IpLeak),
-										logical_and(src < drain, gate - src >= drain - src - Vtp, tI == -Sp*Kp*(drain - src - Vtp - (gate - src)/2.0)*(gate - src) + IpLeak),
-										logical_and(src >= drain, gate - src >= Vtp, tI == IpLeak),
-										logical_and(src >= drain, drain - src <= gate - src - Vtp, tI == 0.5*Sp*Kp*(gate - src - Vtp)*(gate - src - Vtp) + IpLeak),
-										logical_and(src >= drain, drain - src >= gate - src - Vtp, tI == Sp*Kp*(gate - src - Vtp - (drain - src)/2.0)*(drain - src) + IpLeak)))
+		constraints.append(logical_or(logical_and(src < drain, gate - drain >= Vtp, tI == -(src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+										logical_and(src < drain, gate - drain <= Vtp, src - drain <= gate - drain - Vtp, tI == -0.5*Sp*Kp*(gate - drain - Vtp)*(gate - drain - Vtp) - (src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+										logical_and(src < drain, gate - drain <= Vtp, src - drain >= gate - drain - Vtp, tI == -Sp*Kp*(gate - drain - Vtp - (src - drain)/2.0)*(src - drain) - (src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+										logical_and(src >= drain, gate - src >= Vtp, tI == (drain - src)*(2 - (gate - src - Vtp)/Vdd)*(gp*1e-4)),
+										logical_and(src >= drain, gate - src <= Vtp, drain - src <= gate - src - Vtp, tI == 0.5*Sp*Kp*(gate - src - Vtp)*(gate - src - Vtp) + (drain - src)*(2 - (gate - src - Vtp)/Vdd)*(gp*1e-4)),
+										logical_and(src >= drain, gate - src <= Vtp, drain - src >= gate - src - Vtp, tI == Sp*Kp*(gate - src - Vtp - (drain - src)/2.0)*(drain - src) + (drain - src)*(2 - (gate - src - Vtp)/Vdd)*(gp*1e-4))))
 	else:
 		if gate - src >= Vtp:
-			constraints.append(logical_or(logical_and(src < drain, drain - src >= Vtp, tI == IpLeak),
-											logical_and(src < drain, gate - src <= drain - src - Vtp, tI == -0.5*Sp*Kp*(drain - src - Vtp)*(drain - src - Vtp) + IpLeak),
-											logical_and(src < drain, gate - src >= drain - src - Vtp, tI == -Sp*Kp*(drain - src - Vtp - (gate - src)/2.0)*(gate - src) + IpLeak),
-											logical_and(src >= drain, tI == IpLeak)))
+			constraints.append(logical_or(logical_and(src < drain, gate - drain >= Vtp, tI == -(src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+											logical_and(src < drain, gate - drain <= Vtp, src - drain <= gate - drain - Vtp, tI == -0.5*Sp*Kp*(gate - drain - Vtp)*(gate - drain - Vtp) - (src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+											logical_and(src < drain, gate - drain <= Vtp, src - drain >= gate - drain - Vtp, tI == -Sp*Kp*(gate - drain - Vtp - (src - drain)/2.0)*(src - drain) - (src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+											logical_and(src >= drain, tI == (drain - src)*(2 - (gate - src - Vtp)/Vdd)*(gp*1e-4))))
 		else:
-			constraints.append(logical_or(logical_and(src < drain, drain - src >= Vtp, tI == IpLeak),
-										logical_and(src < drain, gate - src <= drain - src - Vtp, tI == -0.5*Sp*Kp*(drain - src - Vtp)*(drain - src - Vtp) + IpLeak),
-										logical_and(src < drain, gate - src >= drain - src - Vtp, tI == -Sp*Kp*(drain - src - Vtp - (gate - src)/2.0)*(gate - src) + IpLeak),
-										logical_and(src >= drain, drain - src <= gate - src - Vtp, tI == 0.5*Sp*Kp*(gate - src - Vtp)*(gate - src - Vtp) + IpLeak),
-										logical_and(src >= drain, drain - src >= gate - src - Vtp, tI == Sp*Kp*(gate - src - Vtp - (drain - src)/2.0)*(drain - src) + IpLeak)))
-
+			constraints.append(logical_or(logical_and(src < drain, gate - drain >= Vtp, tI == -(src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+											logical_and(src < drain, gate - drain <= Vtp, src - drain <= gate - drain - Vtp, tI == -0.5*Sp*Kp*(gate - drain - Vtp)*(gate - drain - Vtp) - (src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+											logical_and(src < drain, gate - drain <= Vtp, src - drain >= gate - drain - Vtp, tI == -Sp*Kp*(gate - drain - Vtp - (src - drain)/2.0)*(src - drain) - (src - drain)*(2 - (gate - drain - Vtp)/Vdd)*(gp*1e-4)),
+											logical_and(src >= drain, drain - src <= gate - src - Vtp, tI == 0.5*Sp*Kp*(gate - src - Vtp)*(gate - src - Vtp) + (drain - src)*(2 - (gate - src - Vtp)/Vdd)*(gp*1e-4)),
+											logical_and(src >= drain, drain - src >= gate - src - Vtp, tI == Sp*Kp*(gate - src - Vtp - (drain - src)/2.0)*(drain - src) + (drain - src)*(2 - (gate - src - Vtp)/Vdd)*(gp*1e-4))))
+	
 	return constraints
 
 def schmittTrigger(inputVoltage, Vtp, Vtn, Vdd, Kn, Kp, Sn, numSolutions = "all"):
@@ -88,9 +87,8 @@ def schmittTrigger(inputVoltage, Vtp, Vtn, Vdd, Kn, Kp, Sn, numSolutions = "all"
 			break
 		allConstraints = []	
 		for i in range(lenV):
-			allConstraints.append(vs[i] >= 0.0)
-			allConstraints.append(vs[i] <= Vdd)
-			allConstraints.append(nIs[i] == 0.0)
+			allConstraints.append(vs[i] >= -0.2)
+			allConstraints.append(vs[i] <= 2.0)
 		allConstraints += nFet(Vtn, Vdd, Kn, Sn, gn, 0.0, inputVoltage, vs[1], tIs[0])
 		allConstraints += nFet(Vtn, Vdd, Kn, Sn, gn, vs[1], inputVoltage, vs[0], tIs[1])
 		allConstraints += nFet(Vtn, Vdd, Kn, Sn, gn, vs[1], vs[0], Vdd, tIs[2])
@@ -100,6 +98,8 @@ def schmittTrigger(inputVoltage, Vtp, Vtn, Vdd, Kn, Kp, Sn, numSolutions = "all"
 		allConstraints.append(nIs[0] == -tIs[4] - tIs[1])
 		allConstraints.append(nIs[1] == -tIs[0] + tIs[1] + tIs[2])
 		allConstraints.append(nIs[2] == -tIs[3] + tIs[5] + tIs[4])
+		for i in range(lenV):
+			allConstraints.append(nIs[i] == 0.0)
 		
 		excludingConstraints = []
 		for solution in allSolutions:
@@ -120,12 +120,12 @@ def schmittTrigger(inputVoltage, Vtp, Vtn, Vdd, Kn, Kp, Sn, numSolutions = "all"
 		#print ("f_sat")
 		#print (f_sat)
 		result = CheckSatisfiability(f_sat, epsilon)
-		#print (result)
+		print (result)
 		if result is None:
 			break
 		hyper = np.zeros((lenV,2))
 		for i in range(lenV):
-			hyper[i,:] = [result[vs[i]].lb() - 1e+10*epsilon, result[vs[i]].ub() + 1e+10*epsilon]
+			hyper[i,:] = [result[vs[i]].lb() - 1e+2*epsilon, result[vs[i]].ub() + 1e+2*epsilon]
 
 		print ("hyper", hyper)
 		allSolutions.append(hyper)
@@ -135,7 +135,7 @@ def schmittTrigger(inputVoltage, Vtp, Vtn, Vdd, Kn, Kp, Sn, numSolutions = "all"
 			break'''
 
 	# categorize solutions found
-	sampleSols, rotatedSols, stableSols, unstableSols = rUtils.categorizeSolutions(allSolutions,model)
+	'''sampleSols, rotatedSols, stableSols, unstableSols = rUtils.categorizeSolutions(allSolutions,model)
 	
 	for solution in allSolutions:
 		print (solution)
@@ -147,8 +147,8 @@ def schmittTrigger(inputVoltage, Vtp, Vtn, Vdd, Kn, Kp, Sn, numSolutions = "all"
 		print ("other member rotationIndices: ")
 		for mi in range(len(rotatedSols[hi])):
 			print (rotatedSols[hi][mi])
-		print ("")
+		print ("")'''
 	end = time.time()
 	print ("time taken", end - start)
 
-schmittTrigger(inputVoltage = 0.0, Vtp = -0.4, Vtn = 0.4, Vdd = 1.8, Kn = 1.5, Kp = -0.75, Sn = (8/3.0), numSolutions = "all")
+schmittTrigger(inputVoltage = 1.2, Vtp = -0.4, Vtn = 0.4, Vdd = 1.8, Kn = 1.5, Kp = -0.75, Sn = (8/3.0), numSolutions = "all")

intervalUtils.py
@@ -151,11 +151,18 @@ def newton(model,soln):
 	h = soln
 	count = 0
 	maxIter = 100
+	boundMap = model.boundMap
+	lenV = len(soln)
+	overallHyper = np.zeros((lenV,2))
+	for i in range(lenV):
+		overallHyper[i,0] = boundMap[i][0][0]
+		overallHyper[i,1] = boundMap[i][1][1]
+	#print ("overallHyper", overallHyper)
 	while count < maxIter and (np.linalg.norm(h) > 1e-8 or count == 0) :
+		#print ("soln", soln)
 		#print ("h", h)
 		_,_,res = model.oscNum(soln)
 		#print ("res", res)
-		#print ("soln", soln)
 		res = -np.array(res)
 		jac = model.jacobian(soln)
 		#print ("res", res)
@@ -165,6 +172,9 @@ def newton(model,soln):
 		except np.linalg.LinAlgError:
 			h = np.linalg.lstsq(jac, res)[0]
 		soln = soln + h
+		#print ("new soln", soln)
+		if np.less(soln, overallHyper[:,0]).any() or np.greater(soln, overallHyper[:,1]).any():
+			return (False, soln)
 		count+=1
 	if count >= maxIter and np.linalg.norm(h) > 1e-8:
 		return(False, soln)
@@ -200,7 +210,9 @@ def checkExistenceOfSolutionGS(model,hyperRectangle):
 		while True:
 			fail = False
 			try:
-				C = np.linalg.inv(jacMidPoint)
+				#print ("midPoint", midPoint)
+				#print ("jacobian in K", jacMidPoint)
+				C = np.linalg.pinv(jacMidPoint)
 				#print ("C", C)
 			except np.linalg.linalg.LinAlgError:
 				fail = True
@@ -293,6 +305,8 @@ def checkExistenceOfSolutionGS(model,hyperRectangle):
 			if gsInterval[i][1] <= startBounds[i][0] or gsInterval[i][1] >= startBounds[i][1]:
 				uniqueSoln = False
 
+		#print (gsInterval[:,0] - startBounds[:,0])
+		#print (startBounds[:,1] - gsInterval[:,1])
 		if uniqueSoln:
 			#print "Hyperrectangle with unique solution found"
 			#print kInterval
@@ -315,6 +329,7 @@ def checkExistenceOfSolutionGS(model,hyperRectangle):
 						#print ("maxDiff", maxDiff)
 						if maxDiff < 1e-9:
 							maxDiff = 1e-9
+						#maxDiff = 0.01
 						#print "maxDiff ", maxDiff
 						gsIntersect[si,0] = soln[1][si] - maxDiff
 						gsIntersect[si,1] = soln[1][si] + maxDiff
@@ -343,7 +358,7 @@ def checkExistenceOfSolution(model,hyperRectangle):
 	iteration = 0
 	while True:
 		#print "iteration number: ", iteration
-		print ("startBounds ", startBounds)
+		#print ("startBounds ", startBounds)
 		midPoint = (startBounds[:,0] + startBounds[:,1])/2.0
 		#midPoint = startBounds[:,0] + (startBounds[:,1] - startBounds[:,0])*0.25
 		#print "midPoint"
@@ -390,8 +405,8 @@ def checkExistenceOfSolution(model,hyperRectangle):
 		kInterval[:,0] = np.minimum(kInterval1, kInterval2)
 		kInterval[:,1] = np.maximum(kInterval1, kInterval2)
 
-		print ("kInterval ")
-		print (kInterval)
+		#print ("kInterval ")
+		#print (kInterval)
 
 		uniqueSoln = True
 		for i in range(numVolts):
@@ -440,7 +455,7 @@ def checkExistenceOfSolution(model,hyperRectangle):
 				constructBiggerHyper = True
 				exampleVolt = (intersect[:,0] + intersect[:,1])/2.0
 				soln = newton(model,exampleVolt)
-				print ("soln ", soln)
+				#print ("soln ", soln)
 				# the new hyper must contain the solution in the middle and enclose old hyper
 				# and then we check for uniqueness of solution in the newer bigger hyperrectangle
 				if soln[0]:
@@ -453,7 +468,7 @@ def checkExistenceOfSolution(model,hyperRectangle):
 						intersect[si,0] = soln[1][si] - maxDiff
 						intersect[si,1] = soln[1][si] + maxDiff
 
-					print ("bigger hyper ", intersect)
+					#print ("bigger hyper ", intersect)
 					startBounds = intersect
 				#print ("after if constructBiggerHyper", constructBiggerHyper)
 			else:

prototype2.py
@@ -45,12 +45,12 @@ solution.
 '''
 def ifFeasibleHyper(hyperRectangle, hyperBound,model):
 	lenV = hyperRectangle.shape[0]
-	#print ("hyperRectangle")
-	#print (hyperRectangle)
+	print ("hyperRectangle")
+	print (hyperRectangle)
 	iterNum = 0
 	while True:
-		#print ("hyperRectangle")
-		#print (hyperRectangle)
+		print ("hyperRectangle")
+		print (hyperRectangle)
 		kResult = intervalUtils.checkExistenceOfSolutionGS(model,hyperRectangle.transpose())
 		#print ("kResult")
 		#print (kResult)
@@ -59,7 +59,7 @@ def ifFeasibleHyper(hyperRectangle, hyperBound,model):
 			return (True, kResult[1])
 
 		if kResult[0] == False and kResult[1] is None:
-			#print ("K operator not feasible")
+			print ("K operator not feasible")
 			return (False, None)
 		#print "kResult"
 		#print kResult
@@ -67,9 +67,9 @@ def ifFeasibleHyper(hyperRectangle, hyperBound,model):
 		#print (hyperRectangle)
 		feasible, newHyperRectangle = model.linearConstraints(hyperRectangle)
 	
-		#print ("newHyperRectangle ", newHyperRectangle)
+		print ("newHyperRectangle ", newHyperRectangle)
 		if feasible == False:
-			#print ("LP not feasible")
+			print ("LP not feasible")
 			return (False, None)
 
 		for i in range(lenV):
@@ -143,8 +143,8 @@ def getFeasibleIntervalIndices(rootCombinationNode,boundMap,hyperBound, model,re
 		return
 
 	intervalIndices = rootCombinationNode.rootArray
-	#print ("intervalIndices", intervalIndices)
-	#print ("boundMap", boundMap)
+	print ("intervalIndices", intervalIndices)
+	print ("boundMap", boundMap)
 	lenV = len(intervalIndices)
 	
 	#feasibility = ifFeasibleOrdering(intervalIndices, boundMap, hyperBound,model)
@@ -166,10 +166,11 @@ def getFeasibleIntervalIndices(rootCombinationNode,boundMap,hyperBound, model,re
 				hyperAlreadyConsidered = True
 				return
 
+	print ("hyperRectangle", hyperRectangle)
 	feasibility = ifFeasibleHyper(hyperRectangle,hyperBound,model)
 
-	#print ("feasibility at level", level)
-	#print (feasibility)
+	print ("feasibility at level", level)
+	print (feasibility)
 
 	if treeVars is not None:
 		global hyperNum
@@ -248,8 +249,8 @@ def bisectHyper(hyperBound,hyperRectangle,bisectingIndex, model,finalHypers, lev
 		global hyperNum
 		parentTree = treeVars[0]
 		parent = treeVars[1]
-	#print "hyperRectangle"
-	#print hyperRectangle
+	print ("hyperRectangle")
+	print (hyperRectangle)
 	lenV = hyperRectangle.shape[0]
 	intervalLength = hyperRectangle[:,1] - hyperRectangle[:,0]
 	bisectingIndex = np.argmax(intervalLength)
@@ -261,16 +262,16 @@ def bisectHyper(hyperBound,hyperRectangle,bisectingIndex, model,finalHypers, lev
 	leftHyper[bisectingIndex][1] = midVal
 
 	rightHyper[bisectingIndex][0] = midVal
-	#print ("leftHyper at level", level)
-	#print (leftHyper)
-	#print ("rightHyper at level", level)
-	#print (rightHyper)
+	print ("leftHyper at level", level)
+	print (leftHyper)
 	feasLeft = ifFeasibleHyper(leftHyper, hyperBound, model)
+	print ("feasLeft")
+	print (feasLeft)
+	print ("rightHyper at level", level)
+	print (rightHyper)
 	feasRight = ifFeasibleHyper(rightHyper, hyperBound, model)
-	#print ("feasLeft")
-	#print (feasLeft)
-	#print ("feasRight")
-	#print (feasRight)
+	print ("feasRight")
+	print (feasRight)
 
 	leftHyperAlreadyConsidered = False
 	rightHyperAlreadyConsidered = False
@@ -364,7 +365,11 @@ def findAndIgnoreNewtonSoln(model, minVal, maxVal, numSolutions, numTrials = 10)
 	for i in range(lenV):
 		overallHyper[i,0] = boundMap[i][0][0]
 		overallHyper[i,1] = boundMap[i][1][1]
+	print ("numTrials in newtons preprocessing", numTrials)
+	start = time.time()
+	numFailures = 0
 	for n in range(numTrials):
+		numFailures += 1
 		if len(allHypers) == numSolutions:
 			break
 		trialSoln = np.random.uniform(minVal, maxVal, (lenV))
@@ -390,12 +395,16 @@ def findAndIgnoreNewtonSoln(model, minVal, maxVal, numSolutions, numTrials = 10)
 						diff[startingIndex] = diff[startingIndex]/2.0
 						startingIndex = (startingIndex + 1)%lenV
 					else:
-						print ("Preprocess: found unique hyper", hyperWithUniqueSoln)
+						end = time.time()
+						print ("Preprocess: found unique hyper ", hyperWithUniqueSoln, "in", end - start, "after", numFailures, "failures")
+						start = time.time()
+						numFailures = 0
 						allHypers.append(hyperWithUniqueSoln)
 						if model.solver is not None:
 							model.ignoreHyperInZ3(hyperWithUniqueSoln)
 						#model.linearConstraints(hyperWithUniqueSoln)
 						break
+	print ("total num hypers found in ", numTrials, " trials is ", len(allHypers))
 	return (allHypers, solutionsFoundSoFar)
 
 
@@ -601,15 +610,15 @@ def schmittTrigger(inputVoltage, treeVars = None, numSolutions = "all", newtonHy
 	print bisectionHypers'''
 
 	'''hyperRectangle = np.zeros((lenV,2))
-	hyperRectangle[0,:] = [1.799999999, 1.800000001]
-	hyperRectangle[1,:] = [1.574999999, 1.575000001]
-	hyperRectangle[2,:] = [1.799999999, 1.800000001]
+	hyperRectangle[0,:] = [0.45, 0.675]
+	hyperRectangle[1,:] = [0.07259583,  0.37749746]
+	hyperRectangle[2,:] = [1.35, 1.8]
 
 	feasibility = ifFeasibleHyper(hyperRectangle, hyperBound,model)
 	#feasibility = intervalUtils.checkExistenceOfSolutionGS(model,hyperRectangle.transpose())
 	print (feasibility)'''
 	#exampleSoln = (hyperRectangle[:,0] + hyperRectangle[:,1])/2.0
-	'''exampleSoln = np.array([1.8, 1.4, 1.8])
+	'''exampleSoln = np.array([1.79997910220505, 1.38792621640977, 1.79999999999999])
 	finalSoln = intervalUtils.newton(model,exampleSoln)
 	print ("finalSoln", finalSoln)'''
 	#print (model.currentFun(0.90732064, 0.09267936))
@@ -648,7 +657,7 @@ def schmittTrigger(inputVoltage, treeVars = None, numSolutions = "all", newtonHy
 	print (allHypers)
 	
 	# categorize solutions found
-	sampleSols, rotatedSols, stableSols, unstableSols = rUtils.categorizeSolutions(allHypers,model)
+	'''sampleSols, rotatedSols, stableSols, unstableSols = rUtils.categorizeSolutions(allHypers,model)
 
 	for hi in range(len(sampleSols)):
 		print ("equivalence class# ", hi)
@@ -667,16 +676,21 @@ def schmittTrigger(inputVoltage, treeVars = None, numSolutions = "all", newtonHy
 
 	print ("")
 	print ("numSolutions, ", len(allHypers))
-	print ("num stable solutions ", len(stableSols))
+	#print ("num stable solutions ", len(stableSols))
 
-	print (model.oscNum(sampleSols[0]))
-	'''for si in range(len(stableSols)):
+	#print (model.oscNum(exampleSoln))
+	for si in range(len(stableSols)):
 		print stableSols[si]'''
 	#print "num unstable solutions ", len(unstableSols)
 	'''for si in range(len(unstableSols)):
-		print unstableSols[si]
+		print unstableSols[si]'''
 	if treeVars is not None:
-		parentTree.show(line_type="ascii-em")'''
+		parentTree.show(line_type="ascii-em")
+	#exampleSoln = np.array([1.73146982670735161, 1.34005499030221964, 1.731472546677237823])
+	#print ("model.oscNum")
+	#print(model.oscNum(exampleSoln))
+	'''finalSoln = intervalUtils.newton(model,exampleSoln)
+	print ("finalSoln", finalSoln)'''
 	endExp = time.time()
 	print ("TOTAL TIME ", endExp - startExp)
 
@@ -805,5 +819,5 @@ def rambusOscillator(numStages, g_cc, treeVars = None, numSolutions="all" , newt
 
 #rambusOscillator(numStages=4, g_cc=4.0, treeVars=None, numSolutions="all" , newtonHypers=True)
 #z3Version(-5.0,2)
-schmittTrigger(inputVoltage = 0.2, treeVars = None, numSolutions = "all", newtonHypers = True)
+schmittTrigger(inputVoltage = 1.2, treeVars = None, numSolutions = "all", newtonHypers = None)
 
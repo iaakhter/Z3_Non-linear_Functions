a531f4e7a8785e8f4a69962066118412032c25fa
iaakhter <iaakhter@colby.edu>
2018-05-29 09:19:52 -0700

    Update schmittMosfet according to new circuit.py

--- schmittMosfet.py
+++ schmittMosfet.py
@@ -12,34 +12,35 @@ class SchmittMosfetMark:
 		self.Vdd = modelParam[2]
 		self.Kn = modelParam[3]
 		self.Kp = modelParam[4]
-		self.Sn = modelParam[5]
-		self.Sp = self.Sn*2.0
+		s0 = 3.0
+		#self.Sn = modelParam[5]
+		#self.Sp = self.Sn*2.0
 		self.inputVoltage = inputVoltage
 
-		nfet = circuit.MosfetModel('nfet', self.Vtn, self.Kn, self.Sn)
-		pfet = circuit.MosfetModel('pfet', self.Vtp, self.Kp, self.Sp)
+		nfet = circuit.MosfetModel('nfet', self.Vtn, self.Kn, "default")
+		pfet = circuit.MosfetModel('pfet', self.Vtp, self.Kp, "default")
 
 		# with the voltage array containing [grnd, Vdd, input, X[0], X[1], X[2]]
 		# where X[0] is the output voltage and X[1] is the voltage at node with 
 		# nfets and X[2] is the voltage at node with pfets
 
 		# src, gate, drain = grnd, input, X[1]
-		m0 = circuit.Mosfet(0, 2, 4, nfet)
+		m0 = circuit.Mosfet(0, 2, 4, nfet, s0)
 
 		# src, gate, drain = X[1], input, X[0]
-		m1 = circuit.Mosfet(4, 2, 3, nfet)
+		m1 = circuit.Mosfet(4, 2, 3, nfet, s0)
 
 		# src, gate, drain = X[1], X[0], Vdd
-		m2 = circuit.Mosfet(4, 3, 1, nfet)
+		m2 = circuit.Mosfet(4, 3, 1, nfet, s0)
 
 		# src, gate, drain = Vdd, input, X[2]
-		m3 = circuit.Mosfet(1, 2, 5, pfet)
+		m3 = circuit.Mosfet(1, 2, 5, pfet, s0*2.0)
 
 		# src, gate, drain = X[2], in, X[0]
-		m4 = circuit.Mosfet(5, 2, 3, pfet)
+		m4 = circuit.Mosfet(5, 2, 3, pfet, s0*2.0)
 
 		# src, gate, drain = X[2], X[0], grnd
-		m5 = circuit.Mosfet(5, 3, 0, pfet)
+		m5 = circuit.Mosfet(5, 3, 0, pfet, s0*2.0)
 
 		self.c = circuit.Circuit([m0, m1, m2, m3, m4, m5])
 
@@ -50,7 +51,8 @@ class SchmittMosfetMark:
 		self.solver = None
 
 	def f(self,V):
-		intervalVal = False
+		myV = [0.0, self.Vdd, self.inputVoltage] + [x for x in V]
+		'''intervalVal = False
 		for i in range(V.shape[0]):
 			if circuit.interval_p(V[i]):
 				intervalVal = True
@@ -64,20 +66,18 @@ class SchmittMosfetMark:
 									V[1,:], 
 									V[2,:]])
 		else:
-			VForSchmitt = np.array([0.0, self.Vdd, self.inputVoltage, V[0], V[1], V[2]])
-		funcVal = self.c.f(VForSchmitt)
+			VForSchmitt = np.array([0.0, self.Vdd, self.inputVoltage, V[0], V[1], V[2]])'''
+		funcVal = self.c.f(myV)
 		return funcVal[3:]
 
 	def oscNum(self,V):
-		VForSchmitt = np.array([0.0, self.Vdd, self.inputVoltage, V[0], V[1], V[2]])
-		funcVal = self.c.f(VForSchmitt)
-		return [None, None, funcVal[3:]]
+		return [None, None, self.f(V)]
 
 	def jacobian(self,V):
 		#print ("calculating jacobian")
-		VForSchmitt = np.array([0.0, self.Vdd, self.inputVoltage, V[0], V[1], V[2]])
+		myV = [0.0, self.Vdd, self.inputVoltage] + [x for x in V]
 		#print ("V", V)
-		jac = self.c.jacobian(VForSchmitt)
+		jac = self.c.jacobian(myV)
 		#print ("jac before")
 		#print (jac)
 		return jac[3:,3:]
@@ -85,16 +85,7 @@ class SchmittMosfetMark:
 
 	def jacobianInterval(self,bounds):
 		#print ("calculating jacobian interval")
-		lowerBound = bounds[:,0]
-		upperBound = bounds[:,1]
-		VForSchmitt = np.array([[0.0, 0.0],
-								[self.Vdd, self.Vdd], 
-								[self.inputVoltage, self.inputVoltage], 
-								[lowerBound[0], upperBound[0]], 
-								[lowerBound[1], upperBound[1]],
-								[lowerBound[2], upperBound[2]]])
-		jac = self.c.jacobian(VForSchmitt)
-		return jac[3:,3:]
+		return self.jacobian(bounds)
 
 class SchmittMosfet:
 	def __init__(self, modelParam, inputVoltage):
